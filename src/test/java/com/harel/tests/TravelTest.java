d215ccf1b3a67f37311c801a040fc8ce5bb31a10
alinakrivizky <krivickaaa@gmail.com>
2026-01-22 01:43:29 +0200

    add

--- src/test/java/com/harel/tests/TravelTest.java
+++ src/test/java/com/harel/tests/TravelTest.java
@@ -8,8 +8,14 @@ import java.time.LocalDate;
 public class TravelTest extends TestBase {
     @Test
     public void travelPositiveTest(){
+        LocalDate departureDate = LocalDate.of(2026, 1, 25);
+        LocalDate returnDate = departureDate.plusDays(30);
+
+        TravelPage travelPage = new TravelPage(driver);
+
         new TravelPage(driver).clickOnFirstBuyButton()
                 .clickOnDestinationButton().clickOnSubmitButton()
-                .selectDate(LocalDate.of(2026, 12, 21));
+                .selectDate(departureDate);
+        travelPage.selectDate(returnDate);
     }
 }

41b7b3b90a9efd8f6abc2b587cd2bcfab5537800
alinakrivizky <krivickaaa@gmail.com>
2026-01-22 23:18:01 +0200

    add

--- src/test/java/com/harel/tests/TestBase.java
+++ src/test/java/com/harel/tests/TestBase.java
@@ -4,6 +4,7 @@ import  org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -16,10 +17,15 @@ public class TestBase {
   public WebDriver driver;
   @BeforeMethod
     public void setup() {
-    driver = new ChromeDriver();
-      driver = new EventFiringDecorator<>(new Listener())
-              .decorate(driver);
-    driver.get("https://digital.harel-group.co.il/travel-policy/wizard/destination");
+      ChromeOptions options = new ChromeOptions();
+      options.addArguments("--headless");
+      options.addArguments("--no-sandbox");
+      options.addArguments("--disable-dev-shm-usage");
+      options.addArguments("--window-size=1920,1080");
+
+      driver = new ChromeDriver(options);
+      driver = new EventFiringDecorator<>(new Listener()).decorate(driver);
+      driver.get("https://digital.harel-group.co.il/travel-policy/wizard/destination");
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }

f10904a0ba86e6e0d939c212c47ae3cab63248ab
alinakrivizky <krivickaaa@gmail.com>
2026-01-24 01:25:20 +0200

    add

--- src/test/java/com/harel/tests/TestBase.java
+++ src/test/java/com/harel/tests/TestBase.java
@@ -21,7 +21,7 @@ public class TestBase {
       WebDriverManager.chromedriver().setup();
 
       ChromeOptions options = new ChromeOptions();
-      //options.addArguments("--headless");
+      options.addArguments("--headless");
       options.addArguments("--no-sandbox");
       options.addArguments("--disable-dev-shm-usage");
       options.addArguments("--window-size=1920,1080");
